3b8764210f0a40ce96acf788f9fb6fd78120c7f5
sunny <sunnywiz@gmail.com>
2023-05-06 23:11:59 -0400

    First pass of trying layout

---
+++ testLayout.js
@@ -0,0 +1,93 @@
+
+const jscad = require('@jscad/modeling')
+const { cuboid, cylinder } = jscad.primitives
+const { translate, rotate, align, scale } = jscad.transforms
+const { expand, offset } = jscad.expansions
+const { colorize } = jscad.colors
+const { union, subtract, intersect } = jscad.booleans
+const { TAU } = jscad.maths.constants
+const { measureAggregateBoundingBox,
+    measureBoundingBox,
+    measureVolume } = jscad.measurements
+
+const arrange = (shapes) => {
+
+    var widest = 0;
+    var deepest = 0;
+    for (var i = 0; i < shapes.length; i++) {
+        var bb = measureBoundingBox(shapes[i]);
+        var sx = bb[1][0] - bb[0][0];
+        var sy = bb[1][1] - bb[0][1];
+        var sz = bb[1][2] - bb[0][2];
+        if (sy > deepest) { deepest = sy; }
+        if (sx > widest) { widest = sx; }
+    }
+
+    var stepx = 0.25;
+    var minwidth = 1;
+
+    var startx = 0;
+    shapes[0] = align({
+        modes: ['min', 'min', 'min'],
+        relativeTo: [startx, 0, 0]
+    }, shapes[0]);
+
+    for (var i = 1; i < shapes.length; i++) {
+        var prevShape = shapes[i - 1];
+
+        var found = 0;
+        for (var tryx = startx;
+            (found == 0) && (tryx < startx + widest);
+            tryx += stepx) {
+            shapes[i] = align({
+                modes: ['min', 'min', 'min'],
+                relativeTo: [tryx, 0, 0]
+            }, shapes[i]);
+
+            var vi = measureVolume(
+                intersect(
+                    prevShape, shapes[i]
+                )
+            );
+            console.log("at " + tryx + " volume is " + vi);
+            if (vi == 0) {
+                // we found a spot where they don't intersect 
+                startx = tryx + minwidth;
+                found = 1;
+            }
+        }
+        if (!found) {
+            console.log("Could not find a spot")
+        } else {
+            shapes[i] =
+                align({
+                    modes: ['min', 'min', 'min'],
+                    relativeTo: [startx, 0, 0]
+                }, shapes[i]);
+        }
+    }
+
+    return shapes;
+}
+
+const main = (params) => {
+
+    var shapes = [];
+    shapes.push(
+        union(
+            align(
+                { modes: ['min', 'max', 'min'] },
+                cuboid({ size: [1, 4, 1] }),
+                cuboid({ size: [4, 1, 1] })
+            )
+        )
+    );
+    shapes.push(cylinder());
+    shapes.push(cuboid({ size: [1, 3, 0.1] }));
+
+    shapes = arrange(shapes);
+
+    return shapes;
+}
+
+module.exports = { main }
