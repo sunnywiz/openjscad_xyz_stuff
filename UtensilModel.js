815160035760dce39cc077456007557bef7da2b5
Sunny Gulati <sunnywiz@gmail.com>
2023-05-07 21:18:29 -0400

    2nd print, starting with big spoon

--- UtensilModel.js
+++ UtensilModel.js
@@ -4,7 +4,7 @@
 const jscad = require('@jscad/modeling')
 const { cuboid, cylinder } = jscad.primitives
 const { translate, rotate, align, scale } = jscad.transforms
-const { expand, offset} = jscad.expansions
+const { expand, offset } = jscad.expansions
 const { colorize } = jscad.colors
 const { union, subtract, intersect } = jscad.booleans
 const { TAU } = jscad.maths.constants
@@ -12,11 +12,19 @@ const { measureAggregateBoundingBox, measureBoundingBox } = jscad.measurements
 
 const { solidByLengths } = require('./solidByLengths.js');
 
+const getParameterDefinitions = () => [
+    {
+        name: 'choice', type: 'radio', caption: 'What To Generate',
+        values: ['rawshape'],
+        captions: ['Raw Shape'], initial: 'rawshape'
+    },
+]
+
 const main = (params) => {
 
     var height = 50;  // height of box, not of items
     var roundness = 5;
-    var fit = 2; 
+    var fit = 2;
     var bottom = 2;
     var rim = 3;
     var between = 2;
@@ -28,55 +36,28 @@ const main = (params) => {
     var dtl = 143.8 / 18;
 
     // using 0,0 as being "skip this block"
-    var knife = solidByLengths(30, h1,
-        [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
-        [2, 2, 2, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0.1]);
-    knife = scale([dtl, dtl, 1], knife);
+    // Y is in lego sizes, X is in mm chunks measured on a lego rig per lego block
+    // divide x in half because its symmetric
+    var bigSpoon = solidByLengths(25, 1,
+        [23, 23, 0, 0, 0, 0, 0, 9,
+            8, 0, 0, 6, 6.5, 0, 7.5, 11,
+            24, 0, 44, 0, 45, 0, 42, 0,
+            28]);
+    bigSpoon = scale([0.5, dtl, 1], bigSpoon);
 
-    var bigfork = solidByLengths(26, h1,
-        [1.5, 0,
-            1.5, 0, 0, 0, 0, 0,
-            .5, 0, 0, 0, 0, 0.5,
-            0, 0, 1.7,
-            0, 0, 0, 0, 0, 1.5, 0, 1.5]);
-    bigfork = scale([dtl, dtl, 1], bigfork);
-
-    var fork = solidByLengths(24, h1,
-        [1.5, 0, 0, 0, 0, 0, 0, 0.5, 0, 0, 0, 0, 0, 0.5, 0, 1.5, 0, 0, 1.6, 0, 0, 0, 0, 0, 1.5]);
-    fork = scale([dtl, dtl, 1], fork);
-
-    // set up on a lego "jigsaw" 
-    // measure the widths in mm at each stage
-    var spoon = solidByLengths(21, h1,
-        [18.6, 18.6, 0, 0
-            , 12, 10.1, 8.5, 7.3,
-            0, 0, 0, 0,
-            7.3, 19, 30, 33.8,
-            33.9, 33.1, 30.0, 25.2,
-            17.4]);
-    spoon = scale([0.5, dtl, 1], spoon);
-
-    var chopstick = solidByLengths(103 + 124, h1,
-        [5.7 / 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3.5 / 2]);
-    // straight measurements, not lego scale.
-    // two chopsticks side by side though
-    chopstick = scale([2,1,1], chopstick); 
-
-    var shapes = [chopstick,
-        knife,
-        knife,
-        bigfork,
-        fork,
-        spoon,
-        spoon
+    var shapes = [bigSpoon
     ];
+
     var c1 = [1, 0, 0, 0.5];
     var c2 = [0, 1, 0, 0.5];
     var c3 = [0, 0, 1, 0.5];
 
+    if (params.choice='rawshape') return shapes; 
+
+    return shapes;  
     // make them rounder
     for (var i = 0; i < shapes.length; i++) {
-        shapes[i] = offset({ delta: (roundness-fit), corners: 'round', segments: 8 }, shapes[i]);
+        shapes[i] = offset({ delta: (roundness - fit), corners: 'round', segments: 8 }, shapes[i]);
         shapes[i] = expand({ delta: roundness, corners: 'round', segments: 8 }, shapes[i]);
     }
 
@@ -125,5 +106,5 @@ const main = (params) => {
     return base;
 }
 
-module.exports = { main }
+module.exports = { main, getParameterDefinitions }
 
