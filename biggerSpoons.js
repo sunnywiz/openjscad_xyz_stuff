73459b77772f8c801d7327c923f1fce458708ec3
Sunny Gulati <sunnygulati@kenmarkeyewear.com>
2023-05-12 12:47:24 -0400

    This version extracted biggerSpoons.js as an example

---
+++ biggerSpoons.js
@@ -0,0 +1,41 @@
+
+const jscad = require('@jscad/modeling')
+const { translateX, scale, mirrorY } = jscad.transforms
+const { solidByLengths } = require('./solidByLengths.js');
+const { layout } = require('./layout.js');
+
+// this could almost be its own thing but i needed solidByLengths, so oh well. 
+
+const getModels = () => { 
+
+        // lego: 143.8mm = 18 dots
+        var dtl = 143.8 / 18;
+
+        // using 0,0 as being "skip this block"
+        // Y is in lego sizes, X is in mm chunks measured on a lego rig per lego block
+        // divide x in half because its symmetric
+        var bigSpoon = solidByLengths(25, 1,
+            [23, 23, 0, 0, 0, 0, 0, 9,
+                8, 0, 0, 6, 6.5, 0, 7.5, 11,
+                24, 0, 44, 0, 45, 0, 42, 0,
+                28]);
+        bigSpoon = scale([0.5, dtl, 1], bigSpoon);
+    
+        var server = solidByLengths(28, 1,
+            [26, 26, 0, 0, 0, 0, 0, 0,
+                9.5, 0, 0, 0, 0, 0, 0, 0,
+                8, 14, 34, 39, 41, 41, 41, 40,
+                39, 0, 0, 29]);
+        server = scale([0.5, dtl, 1], server);
+    
+        var shapes = [bigSpoon, translateX(50,mirrorY(server))];
+
+        return shapes;     
+}
+
+const main = (params) => {
+    return layout({separation:10},getModels()); 
+}
+
+module.exports = { getModels, main }
+
--- parametricTray.js
+++ parametricTray.js
@@ -3,20 +3,27 @@
 
 const jscad = require('@jscad/modeling')
 const { cuboid, cylinder } = jscad.primitives
-const { translate, rotate, align, scale, scaleZ, mirrorY } = jscad.transforms
-const { expand, offset } = jscad.expansions
+const { translate, rotate, align, scaleZ} = jscad.transforms
+const { expand} = jscad.expansions
 const { colorize } = jscad.colors
 const { union, subtract, intersect } = jscad.booleans
 const { TAU } = jscad.maths.constants
-const { measureAggregateBoundingBox, measureBoundingBox, measureDimensions } = jscad.measurements
+const { measureAggregateBoundingBox, measureDimensions } = jscad.measurements
 
-const { solidByLengths } = require('./solidByLengths.js');
 const { layout } = require('./layout.js');
 
 const getParameterDefinitions = () => [
+    { 
+        name: 'whichModel',
+        type:'choice',
+        values:[
+            'biggerSpoons.js',
+        ],
+        caption:'Which items to build a tray for?',
+        initial:'biggerspoons.js'},
     { name: 'height', type: 'number', initial: 50, min: 1.0, max: 100.0, step: 5, caption: 'Height of Tray' },    
     {
-        name: 'choice', type: 'radio', caption: 'What To Generate',
+        name: 'choice', type: 'choice', caption: 'What To Generate',
         values: ['init', 'rawshape', 'roundedcuts', 'outlinedholders', 'tray','traywithcutout'],
         captions: ['Initial - nothing', 'Raw Shapes Arranged', 'Rounded Cuts Arranged', 'outlinedholders', 'Full Tray','Tray with cutout'],
         initial: 'init'
@@ -26,39 +33,18 @@ const getParameterDefinitions = () => [
 
 const main = (params) => {
 
+    if (params.choice == 'init') return cuboid();
+
+    const { getModels }  = require('./'+params.whichModel);
+
+    var shapes = getModels(); 
+
     var height = params.height;  // height of box, not of items
     var roundness = 3;
     var fit = 2;
     var bottom = 3;
     var rim = 3;
     var between = 2;
-
-    if (params.choice == 'init') return cuboid();
-
-    var h1 = height - roundness - bottom; // height of these items
-    // other items might have their own height
-
-    // lego: 143.8mm = 18 dots
-    var dtl = 143.8 / 18;
-
-    // using 0,0 as being "skip this block"
-    // Y is in lego sizes, X is in mm chunks measured on a lego rig per lego block
-    // divide x in half because its symmetric
-    var bigSpoon = solidByLengths(25, 1,
-        [23, 23, 0, 0, 0, 0, 0, 9,
-            8, 0, 0, 6, 6.5, 0, 7.5, 11,
-            24, 0, 44, 0, 45, 0, 42, 0,
-            28]);
-    bigSpoon = scale([0.5, dtl, 1], bigSpoon);
-
-    var server = solidByLengths(28, 1,
-        [26, 26, 0, 0, 0, 0, 0, 0,
-            9.5, 0, 0, 0, 0, 0, 0, 0,
-            8, 14, 34, 39, 41, 41, 41, 40,
-            39, 0, 0, 29]);
-    server = scale([0.5, dtl, 1], server);
-
-    var shapes = [bigSpoon, mirrorY(server)];
 
     // for debugging
     var c1 = [1, 0, 0, 0.5];
